d2a33059c307cb9ed56e3fd7eb898b9a535656d7
davidag90 <info@dagdev.com.ar>
2025-09-11 12:05:36 -0300

    Update API_CAPACITACIONES_URL to revert per_page limit to 100

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,7 +1,7 @@
 const SITE_URL = envParams.SITE_URL;
 const API_BASE_URL = SITE_URL + "wp-json/wp/v2/";
 const THEME_URL = SITE_URL + "wp-content/themes/coc-theme/";
-const API_CAPACITACIONES_URL = API_BASE_URL + "capacitaciones?_embed&per_page=150";
+const API_CAPACITACIONES_URL = API_BASE_URL + "capacitaciones?_embed&per_page=100";
 const API_BENEFICIOS_URL = API_BASE_URL + "beneficio?_embed&per_page=100&acf_format=standard";
 const API_SOCIEDADES_URL = API_BASE_URL + "sociedad?_embed&per_page=100&acf_format=standard";
 const API_MEDIA_BASE = API_BASE_URL + "media/";
