938de6c520db97bf4bf5fb0c994217e5ad647f69
davidag90 <info@dagdev.com.ar>
2025-10-04 16:35:43 -0300

    Update API URLs in env.js to use WP_API_BASE_URL and bump version to 2.1.7

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,13 +1,14 @@
 const SITE_URL = envParams.SITE_URL;
 const API_BASE_URL = SITE_URL + "wp-json/coc-api/v1/";
+const WP_API_BASE_URL = SITE_URL + "wp-json/coc-api/v1/";
 const THEME_URL = SITE_URL + "wp-content/themes/coc-theme/";
 const API_CAPACITACIONES_VIGENTES_URL =
   API_BASE_URL + "capacitaciones/vigentes";
 const API_CAPACITACIONES_INICIADAS_URL =
   API_BASE_URL + "capacitaciones/iniciadas";
 const API_BENEFICIOS_URL =
-  API_BASE_URL + "beneficio?_embed&per_page=100&acf_format=standard";
+  WP_API_BASE_URL + "beneficio?_embed&per_page=100&acf_format=standard";
 const API_SOCIEDADES_URL =
-  API_BASE_URL + "sociedad?_embed&per_page=100&acf_format=standard";
+  WP_API_BASE_URL + "sociedad?_embed&per_page=100&acf_format=standard";
 const API_MEDIA_BASE = API_BASE_URL + "media/";
 const CHECKOUT_URL = SITE_URL + "finalizar-compra/?add-to-cart=";
