95733a53a3a842dcb25e9a785ec529260a564435
davidag90 <info@dagdev.com.ar>
2025-10-04 16:37:36 -0300

    Update WP_API_BASE_URL in env.js to point to the correct WordPress API endpoint

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,6 +1,6 @@
 const SITE_URL = envParams.SITE_URL;
 const API_BASE_URL = SITE_URL + "wp-json/coc-api/v1/";
-const WP_API_BASE_URL = SITE_URL + "wp-json/coc-api/v1/";
+const WP_API_BASE_URL = SITE_URL + "wp-json/wp/v2/";
 const THEME_URL = SITE_URL + "wp-content/themes/coc-theme/";
 const API_CAPACITACIONES_VIGENTES_URL =
   API_BASE_URL + "capacitaciones/vigentes";
