2d42cf76500d8fd3f5353f5d0affaec9f273b917
davidag90 <info@dagdev.com.ar>
2025-10-07 12:56:23 -0300

    Refactor API URLs in env.js to ensure consistent trailing slashes

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,13 +1,10 @@
 const SITE_URL = envParams.SITE_URL;
-const API_BASE_URL = SITE_URL + "wp-json/coc-api/v1/";
-const WP_API_BASE_URL = SITE_URL + "wp-json/wp/v2/";
-const THEME_URL = SITE_URL + "wp-content/themes/coc-theme/";
-const API_CAPACITACIONES_VIGENTES_URL =
-  API_BASE_URL + "capacitaciones/vigentes";
-const API_CAPACITACIONES_INICIADAS_URL =
-  API_BASE_URL + "capacitaciones/iniciadas";
-const API_BENEFICIOS_URL = API_BASE_URL + "beneficios";
-const API_SOCIEDADES_URL =
-  WP_API_BASE_URL + "sociedad?_embed&per_page=100&acf_format=standard";
-const API_MEDIA_BASE = API_BASE_URL + "media/";
-const CHECKOUT_URL = SITE_URL + "finalizar-compra/?add-to-cart=";
+const API_BASE_URL = SITE_URL + "/wp-json/coc-api/v1";
+const WP_API_BASE_URL = SITE_URL + "/wp-json/wp/v2";
+const THEME_URL = SITE_URL + "/wp-content/themes/coc-theme/";
+const API_CAPACITACIONES_VIGENTES_URL = `${API_BASE_URL}/capacitaciones/vigentes`;
+const API_CAPACITACIONES_INICIADAS_URL = `${API_BASE_URL}/capacitaciones/iniciadas`;
+const API_BENEFICIOS_URL = `${API_BASE_URL}/beneficios`;
+const API_SOCIEDADES_URL = `${API_BASE_URL}/sociedades`;
+const API_MEDIA_BASE = WP_API_BASE_URL + "/media/";
+const CHECKOUT_URL = SITE_URL + "/finalizar-compra/?add-to-cart=";
